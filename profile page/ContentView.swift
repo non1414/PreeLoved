1d4d0208a4ab24d6082e27a62173bf0ba803236c
non1414 <nouf22228@gmail.com>
2023-02-09 11:18:28 +0300

    Initial Commit

---
+++ profile page/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  profile page
+//
+//  Created by نوف بخيت الغامدي on 18/07/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ profile page/profile_pageApp.swift
@@ -0,0 +1,17 @@
+//
+//  profile_pageApp.swift
+//  profile page
+//
+//  Created by نوف بخيت الغامدي on 18/07/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct profile_pageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
